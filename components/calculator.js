28dcf15458e80a2fe0cfbdf40619fc769ec11686
Logan Jones <loganemilyjones@mac.com>
2021-04-30 11:29:49 -0700

    initial setup

---
+++ components/calculator.js
@@ -0,0 +1,12 @@
+export default function Calculator() {
+    return (
+        <div className="flex flex-wrap items-center justify-around mt-6">
+        <div className="p-6 mt-6 text-left border w-96 rounded-xl">
+          <h3 className="text-2xl">Title</h3>
+          <p className="mt-4 text-xl">
+            Text text text text
+          </p>
+        </div>
+      </div>
+    )
+}
